b6bca7b6bd5e417dca0f6e587cd96e9f9c00e24d
Rudy Chaney <rudy@rudeluvdesigns.com>
2016-06-22 13:42:25 -0700

    bugfixes

--- dist/basic-ass-deploy.js
+++ dist/basic-ass-deploy.js
@@ -17,8 +17,8 @@ var _fs2 = _interopRequireDefault(_fs);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 exports.default = new Promise(function (resolve, reject) {
-  if (_fs2.default.existsSync('../../bad.json')) {
-    _fs2.default.readFile('../../bad.json', 'utf8', function (err, config) {
+  if (_fs2.default.existsSync('../../../bad.json')) {
+    _fs2.default.readFile('../../../bad.json', 'utf8', function (err, config) {
       if (err) throw err;
 
       config = JSON.parse(config);
