5972621aa43dccc23ffd75eb59e8437611d324ff
Rudy Chaney <rudy@rudeluvdesigns.com>
2016-06-22 17:56:21 -0700

    need to figure out testing with sample config

--- test/cli.js
+++ test/cli.js
@@ -1,34 +1,12 @@
 var chai = require('chai');
 var expect = chai.expect;
 var should = chai.should();
-var jsonfile = require('jsonfile');
 
-var sampleConfig = {
-  "ssh": {
-    "host": "api.yourhost.com",
-    "username": "ubuntu",
-    "privateKey": "~/yoursshkey.pem"
-  },
-  "stack": {
-    "process_manager": "pm2",
-    "app_name": "my-api",
-    "directory": "/srv/my-api"
-  },
-  "code": {
-    "repo": "",
-    "branch": "master"
-  }
-};
 
 describe('test', function() {
 
   it('Start', function() {
-    jsonfile.writeFile('bad.json', sampleConfig, function (err) {
-      console.log('writing sample config...');
-      if (err) throw err;
-
-      var bad = require('../dist/basic-ass-deploy');
-      done();
+    return true;
     });
   });
 
